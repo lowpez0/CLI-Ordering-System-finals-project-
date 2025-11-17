f5216230378d0cb09ae25f3b84c5ca6535036a4e
Dhan <sadscientist101@gmail.com>
2025-11-17 17:27:07 +0800

    create csv file for product list

---
+++ src/CLInterface.java
@@ -0,0 +1,13 @@
+public class CLInterface {
+
+    public CLInterface() {}
+
+    public void start() {
+        System.out.println("\nBuildMyPc" +
+                "\n\n1.View Items\n2.Vouchers\n3.Order History" +
+                "\n4.View Cart\n5.Checkout");
+    }
+
+
+}
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        CLInterface cli = new CLInterface();
+
+        cli.start();
+    }
+}
